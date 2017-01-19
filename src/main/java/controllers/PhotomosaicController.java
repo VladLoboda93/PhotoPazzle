41699340e8fa33ac4aa2fd82610ea78b02d29c22
Maksim Chepelev <chepelev.maksim@gmail.com>
2017-01-19 23:07:41 +0200

    commit for test

--- src/main/java/controllers/PhotomosaicController.java
+++ src/main/java/controllers/PhotomosaicController.java
@@ -15,19 +15,9 @@ import controllers.actions.Action;
 public class PhotomosaicController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
-	
-
-
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
 		Action currentAction = AbstractActionFactory.getInstance().getAction(request);
-		currentAction.execute(request, response);
-
-		
+		currentAction.execute(request, response);	
 	}
-	
-	
-
 }
