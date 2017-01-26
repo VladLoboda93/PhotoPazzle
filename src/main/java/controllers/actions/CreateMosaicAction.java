748d774e24d82d13bd6133042cf398a8c5667fe3
EvgenyKoval <shiller1993@gmail.com>
2017-01-26 16:46:31 +0300

    Update CreateMosaicAction.java

--- src/main/java/controllers/actions/CreateMosaicAction.java
+++ src/main/java/controllers/actions/CreateMosaicAction.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-import controllers.Executor;
 import net.lingala.zip4j.exception.ZipException;
 import util.DirectoryHandler;
 
